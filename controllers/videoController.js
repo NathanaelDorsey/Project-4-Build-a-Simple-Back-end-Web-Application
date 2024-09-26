f6b69a161ef64420ef051881fdd8f9ede4aa67cf
Nathanael Dorsey <146008569+NathanaelDorsey@users.noreply.github.com>
2024-09-26 10:53:39 -0400

    Update videoController.js

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -68,11 +68,3 @@ exports.displayDashboard = (req, res) => {
 
     res.render('dashboard', { videos: filteredVideos });
 };
-exports.logoutUser = (req, res) => {
-    req.session.destroy((err) => {
-        if (err) {
-            return res.redirect('/video/dashboard/all');
-        }
-        res.redirect('/users/login');
-    });
-};
