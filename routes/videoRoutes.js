5c70a65fa80dc76a128eb6f43dd191fb58b78a38
Nathanael Dorsey <146008569+NathanaelDorsey@users.noreply.github.com>
2024-09-26 10:31:16 -0400

    Update videoRoutes.js

--- routes/videoRoutes.js
+++ routes/videoRoutes.js
@@ -5,6 +5,7 @@ const videoController = require('../controllers/videoController');
 
 router.get('/newVideo', videoController.newVideoForm);
 router.post('/newVideo', videoController.addNewVideo);
+router.post('/users/logout', videoController.logoutUser);
 router.get('/dashboard/:videofilter', videoController.displayDashboard);
 
 
